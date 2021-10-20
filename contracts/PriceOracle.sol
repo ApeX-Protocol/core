040cf7c0fc2dc5c734095e72b35439ebf7d54b11
Keegan Lee <keegan@SG08222ML.local>
2021-10-20 17:38:59 +0800

    update PriceOracle

--- contracts/PriceOracle.sol
+++ contracts/PriceOracle.sol
@@ -1,9 +1,14 @@
 pragma solidity ^0.8.0;
 
 contract PriceOracle is IPriceOracle {
-    address uniswapV2Factory;
+    using SafeMath for uint256;
+
+    address public uniswapV2Factory;
+    uint8 public decimals;
 
     function getSpotPrice(address baseToken, address quoteToken) external view returns (uint256 price) {
-        
+        (uint256 reserveBase, uint256 reserveQuote) = UniswapV2Library.getReserves(uniswapV2Factory, baseToken, quoteToken);
+        price = reserveBase.mul(1e18).div(reserveQuote); 
     }
+
 }
