c870bfa9a1b26521708247c1c20a57212f4ac926
Keegan Lee <keegan@SG08222ML.local>
2021-10-20 22:55:57 +0800

    调整PriceOracle接口和实现

--- contracts/PriceOracle.sol
+++ contracts/PriceOracle.sol
@@ -1,14 +1,25 @@
 pragma solidity ^0.8.0;
 
+import "./interfaces/IPriceOracle.sol";
+import "./libraries/UniswapV2Library.sol";
+
 contract PriceOracle is IPriceOracle {
-    using SafeMath for uint256;
-
     address public uniswapV2Factory;
-    uint8 public decimals;
 
-    function getSpotPrice(address baseToken, address quoteToken) external view returns (uint256 price) {
-        (uint256 reserveBase, uint256 reserveQuote) = UniswapV2Library.getReserves(uniswapV2Factory, baseToken, quoteToken);
-        price = reserveBase.mul(1e18).div(reserveQuote); 
+    constructor(address _uniswapV2Facroty) public {
+        uniswapV2Factory = _uniswapV2Facroty;
     }
 
+    function quote(
+        address baseToken,
+        address quoteToken,
+        uint256 baseAmount
+    ) external view returns (uint256 quoteAmount) {
+        (uint256 reserveBase, uint256 reserveQuote) = UniswapV2Library.getReserves(
+            uniswapV2Factory,
+            baseToken,
+            quoteToken
+        );
+        quoteAmount = UniswapV2Library.quote(baseAmount, reserveBase, reserveQuote);
+    }
 }
--- contracts/Staking.sol
+++ contracts/Staking.sol
@@ -1,5 +1,49 @@
 pragma solidity ^0.8.0;
 
-contract Staking {
-    
+contract Staking is IStaking {
+    address public factory;
+    address public config;
+    address public amm;
+    uint256 public totalSupply;
+    mapping(address => uint256) public balanceOf;
+
+    bool private locked = false;
+    modifier lock() {
+        require(locked == false, "Staking: LOCKED");
+        locked = true;
+        _;
+        locked = false;
+    }
+
+    constructor(address _config, address _amm) public {
+        factory = msg.sender;
+        config = _config;
+        amm = _amm;
+    }
+
+    function stake(uint256 amount) external lock {
+        require(amount > 0, "Staking: Cannot stake 0");
+        totalSupply += amount;
+        balanceOf[msg.sender] += amount;
+        IAmm(amm).safeTransferFrom(msg.sender, address(this), amount);
+        emit Staked(msg.sender, amount);
+    }
+
+    function stakeWithPermit(
+        uint256 amount,
+        uint256 deadline,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) external lock {
+        require(amount > 0, "Staking: Cannot stake 0");
+        totalSupply += amount;
+        balanceOf[msg.sender] += amount;
+
+        // permit
+        IUniswapV2ERC20(address(amm)).permit(msg.sender, address(this), amount, deadline, v, r, s);
+
+        stakingToken.safeTransferFrom(msg.sender, address(this), amount);
+        emit Staked(msg.sender, amount);
+    }
 }
