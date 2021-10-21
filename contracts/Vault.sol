ed05afe61bde5c13286e0e70670ba0a36713051e
riverman123 <yanyanho126@gmail.com>
2021-10-21 11:36:24 +0800

    add univ2 skd-core

--- contracts/Vault.sol
+++ contracts/Vault.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.0;
 
 import "./interfaces/IERC20.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
-import "./libraries/Ownable.sol";
+import "./utils/Ownable.sol";
 import "hardhat/console.sol";
 
 contract Vault is ReentrancyGuard, Ownable {
