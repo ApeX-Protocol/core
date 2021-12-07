105388e5d3c0a352c18aac10daa1641aa7288bda
keeganlee <keegan.lee704@gmail.com>
2021-12-07 15:12:27 +0800

    upgrade PriceOracle

--- contracts/core/PriceOracle.sol
+++ contracts/core/PriceOracle.sol
@@ -6,32 +6,38 @@ import "./interfaces/IConfig.sol";
 import "./interfaces/IPriceOracle.sol";
 import "./interfaces/uniswapV3/IUniswapV3Factory.sol";
 import "./interfaces/uniswapV3/IUniswapV3Pool.sol";
+import "./interfaces/uniswapV2/IUniswapV2Factory.sol";
+import "./interfaces/uniswapV2/IUniswapV2Pair.sol";
 import "../libraries/FullMath.sol";
 import "../libraries/UniswapV3TwapGetter.sol";
 import "../libraries/FixedPoint96.sol";
 
 contract PriceOracle is IPriceOracle {
-    address public immutable uniswapV3Factory;
-    uint24[3] public fees;
+    address public immutable v3Factory;
+    address public immutable v2Factory;
+    address public immutable WETH;
+    uint24[3] public v3Fees;
 
-    constructor(address uniswapV3Factory_) {
-        uniswapV3Factory = uniswapV3Factory_;
-        fees[0] = 500;
-        fees[1] = 3000;
-        fees[2] = 10000;
+    constructor(address v3Factory_, address v2Factory_, address WETH_) {
+        v3Factory = v3Factory_;
+        v2Factory = v2Factory_;
+        WETH = WETH_;
+        v3Fees[0] = 500;
+        v3Fees[1] = 3000;
+        v3Fees[2] = 10000;
     }
 
-    function quote(
+    function quoteFromV3(
         address baseToken,
         address quoteToken,
         uint256 baseAmount
-    ) public view override returns (uint256 quoteAmount) {
+    ) public view returns (uint256 quoteAmount) {
         uint128 maxLiquidity;
         uint160 sqrtPriceX96;
         address pool;
         uint128 liquidity;
-        for (uint256 i = 0; i < fees.length; i++) {
-            pool = IUniswapV3Factory(uniswapV3Factory).getPool(baseToken, quoteToken, fees[i]);
+        for (uint256 i = 0; i < v3Fees.length; i++) {
+            pool = IUniswapV3Factory(v3Factory).getPool(baseToken, quoteToken, v3Fees[i]);
             if (pool == address(0)) continue;
             liquidity = IUniswapV3Pool(pool).liquidity();
             // use the max liquidity pool as oracle price source
@@ -40,13 +46,57 @@ contract PriceOracle is IPriceOracle {
                 sqrtPriceX96 = UniswapV3TwapGetter.getSqrtTwapX96(pool, 60);
             }
         }
-        require(sqrtPriceX96 > 0, "PriceOracle.quote: NO_PRICE");
+        if (sqrtPriceX96 == 0) return 0;
         // priceX96 = token1/token0, this price is scaled by 2^96
         uint256 priceX96 = UniswapV3TwapGetter.getPriceX96FromSqrtPriceX96(sqrtPriceX96);
         if (baseToken == IUniswapV3Pool(pool).token0()) {
             quoteAmount = FullMath.mulDiv(baseAmount, priceX96, FixedPoint96.Q96);
         } else {
             quoteAmount = FullMath.mulDiv(baseAmount, FixedPoint96.Q96, priceX96);
+        }
+    }
+
+    function quoteFromV2(
+        address baseToken,
+        address quoteToken,
+        uint256 baseAmount
+    ) public view returns (uint256 quoteAmount) {
+        address pair = IUniswapV2Factory(v2Factory).getPair(baseToken, quoteToken);
+        if (pair == address(0)) return 0;
+        (uint112 reserve0, uint112 reserve1, ) = IUniswapV2Pair(pair).getReserves();
+        if (baseToken == IUniswapV2Pair(pair).token0()) {
+            quoteAmount = FullMath.mulDiv(baseAmount, reserve1, reserve0);
+        } else {
+            quoteAmount = FullMath.mulDiv(baseAmount, reserve0, reserve1);
+        }
+    }
+
+    function quoteFromHybrid(
+        address baseToken, 
+        address quoteToken,
+        uint256 baseAmount
+    ) public view returns (uint256 quoteAmount) {
+        uint256 wethAmount = quoteFromV3(baseToken, WETH, baseAmount);
+        if (wethAmount == 0) {
+            wethAmount = quoteFromV2(baseToken, WETH, baseAmount);
+        }
+        quoteAmount = quoteFromV3(WETH, quoteToken, wethAmount);
+        if (quoteAmount == 0) {
+            quoteAmount = quoteFromV2(WETH, quoteToken, wethAmount);
+        }
+    }
+
+    function quote(
+        address baseToken,
+        address quoteToken,
+        uint256 baseAmount
+    ) public view override returns (uint256 quoteAmount) {
+        quoteAmount = quoteFromV3(baseToken, quoteToken, baseAmount);
+        if (quoteAmount == 0) {
+            quoteAmount = quoteFromV2(baseToken, quoteToken, baseAmount);
+        } 
+        if (quoteAmount == 0) {
+            quoteAmount = quoteFromHybrid(baseToken, quoteToken, baseAmount);
         }
     }
 
---
+++ contracts/core/interfaces/uniswapV2/IUniswapV2Factory.sol
@@ -0,0 +1,4 @@
+pragma solidity ^0.8.0;
+interface IUniswapV2Factory {
+    function getPair(address tokenA, address tokenB) external view returns (address pair);
+}
