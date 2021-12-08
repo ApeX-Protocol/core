5cdb5ebcad77ab46203b4a82548bf98e99ee5205
riverman123 <yanyanho126@gmail.com>
2021-12-08 14:40:35 +0800

    optimist the nftsquid contract

--- test/nftSquid.js
+++ test/nftSquid.js
@@ -10,7 +10,8 @@ describe("nftSquid contract", function () {
   let nftSquid;
   let erc20;
   let exp1 = ethers.BigNumber.from("10").pow(18);
-  let ct;
+  let players  = 456;
+  
   let args;
 
   beforeEach(async function () {
@@ -19,87 +20,57 @@ describe("nftSquid contract", function () {
 
     const NftSquid = await ethers.getContractFactory("NftSquid");
     nftSquid = await NftSquid.deploy(erc20.address);
-    ct = currentTimestamp();
-    console.log("ct:", ct);
-
-    await nftSquid.setStartTime(ct + 500);
-    await erc20.transfer(nftSquid.address, ethers.BigNumber.from("10000000").mul(exp1));
+    console.log("nftSquid: ", nftSquid.address);
+    
+    
   });
 
-  // it("burn 0", async function () {
-  //   let overrides = {
-  //     value: ethers.utils.parseEther("0.01")
-  //   }
-  //    await nftSquid.claimApeXNFT(overrides);
 
-  //   let oldRemainOwners = await nftSquid.remainOwners();
-  //   await network.provider.send("evm_increaseTime", [60])
-  //   await network.provider.send("evm_mine")
-  //   await nftSquid.burn(0);
-  //   expect(await nftSquid.remainOwners()).to.be.equal(oldRemainOwners - 1);
-  // });
-  // it("burn 456 in  0 month", async function () {
-  //   let i =0;
-  //   let overrides = {
-  //     value: ethers.utils.parseEther("0.01")
-  //   }
-  //   for( i =0 ;i< 456; i++) {
-  //    await nftSquid.claimApeXNFT(overrides);
-  //   }
-  //   await network.provider.send("evm_increaseTime", [600])
-  //   await network.provider.send("evm_mine")
-  //   console.log("mint successfully.");
-  //    i =0;
-
-  //   for( i =0 ;i< 456; i++) {
-
-  //   let tx = await nftSquid.burn(i);
-  //   let txReceipt = await tx.wait();
-  //    args = txReceipt["events"][2].args;
-  //   // if(i%100==0) {
-  //   console.log("id : ", args[0].toString());
-  //   console.log("amount ", args[1].div(exp1).toString());
-  //  // }
-
-  //   }
-  //   expect(args[1].div(exp1).toString()).to.be.equal('165548');
-  //   expect(args[0].toString()).to.be.equal('455');
-  // });
-
-  it("burn 456 in  1 month", async function () {
+  it("burn  456 in  6 month", async function () {
+    let dateTime = new Date();
+    let ct =  Math.floor(dateTime / 1000);
+    console.log("ct:", ct);
+    await nftSquid.setStartTime(ct + 500);
+    let  balance = await ethers.provider.getBalance(owner.address);
+    console.log("balance: ", balance.div(exp1).toString());
     let i = 0;
     let overrides = {
-      value: ethers.utils.parseEther("0.01"),
+      value: ethers.utils.parseEther("2.5"),
     };
-    for (i = 0; i < 456; i++) {
+   
+    for (i = 0; i < players; i++) {
       await nftSquid.claimApeXNFT(overrides);
     }
-    await network.provider.send("evm_increaseTime", [2592000 * 6]);
-    await network.provider.send("evm_mine");
-    console.log("mint successfully.");
+
+    await erc20.transfer(nftSquid.address, ethers.BigNumber.from(15000*players).mul(exp1));
+
+    // await ethers.provider.send('evm_setNextBlockTimestamp', [ct+600])
+    // await network.provider.send("evm_mine");
+    console.log("mint nft successfully.");
     i = 0;
 
-    for (i = 0; i < 456; i++) {
+    for (i = 0; i < players; i++) {
+      await ethers.provider.send('evm_setNextBlockTimestamp', [ct+33696 +33696*i])
+      await network.provider.send("evm_mine");
       let tx = await nftSquid.burn(i);
       let txReceipt = await tx.wait();
       args = txReceipt["events"][2].args;
-      // if(i%100==0) {
-      console.log("id : ", args[0].toString());
-      console.log("amount ", args[1].div(exp1).toString());
-      // }
-    }
-    expect(args[1].div(exp1).toString()).to.be.equal("75221");
+      
+      console.log("id : ", args[0].add(1).toString());
+      console.log("amount ", (args[1].div(exp1).sub(10000)).toString());
+     }
+    expect(args[1].div(exp1).toString()).to.be.equal("103864");
     expect(args[0].toString()).to.be.equal("455");
+    let  balanceBefore = await ethers.provider.getBalance(owner.address);
+    
+
+    await nftSquid.withdrawETH(owner.address);
+    let  balanceAfter =  await ethers.provider.getBalance(owner.address);
+   console.log("balanceAfter: ", balanceAfter.div(exp1).toString());
+   let apexAmount = await erc20.balanceOf(nftSquid.address);
+   console.log("APEX token Amount: ", apexAmount);
+    
   });
 });
 
-function currentTimestamp() {
-  let dateTime = new Date();
-  return Math.floor(dateTime / 1000);
-}
-
-//ms
-async function sleep(ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
 
