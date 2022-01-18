f446c5cc04be948a1beb7059d6295dd0c571b110
yuanye.li <yuanye.li@bybit.com>
2022-01-18 14:13:16 +0800

    refactor: update nft

--- contracts/NFT/ApeXVIPNFT.sol
+++ contracts/NFT/ApeXVIPNFT.sol
@@ -7,17 +7,14 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "../core/interfaces/IERC20.sol";
 
 contract ApeXVIPNFT is ERC721PresetMinterPauserAutoId, Ownable {
+    uint256 constant nftMaxAmount = 20;
     uint256 public price = 40 ether;
-    uint256 public totalEth = 0;
-    uint256 public remainOwners = 20;
-    uint256 public id = 0;
-    uint256 public startTime;
+    uint256 public id;
     mapping(address => bool) public whitelist;
     mapping(address => bool) public buyer;
-
-    uint256 public cliff;
-    uint256 public start;
-    uint256 public duration;
+    uint256 public startTime;
+    uint256 public cliffTime;
+    uint256 public endTime;
     uint256 public totalAmount;
     mapping(address => uint256) public claimed;
     address public token;
@@ -33,76 +30,74 @@ contract ApeXVIPNFT is ERC721PresetMinterPauserAutoId, Ownable {
         uint256 _cliff,
         uint256 _duration
     ) ERC721PresetMinterPauserAutoId(_name, _symbol, _baseTokenURI) {
+        require(_cliff < _duration, "INVALID_CLIFF_OR_DURATION");
         token = _token;
         startTime = _startTime;
-        duration = _startTime + _duration;
-        cliff = _startTime + _cliff;
+        cliffTime = _startTime + _cliff;
+        endTime = _startTime + _duration;
     }
 
     function setTotalAmount(uint256 _totalAmount) external onlyOwner {
         totalAmount = _totalAmount;
     }
 
-    function addManyToWhitelist(address[] calldata _beneficiaries) external onlyOwner {
+    function addToWhitelist(address[] calldata _beneficiaries) external onlyOwner {
         for (uint256 i = 0; i < _beneficiaries.length; i++) {
             whitelist[_beneficiaries[i]] = true;
         }
     }
 
-    function removeFromWhitelist(address _beneficiary) public onlyOwner {
-        _removeFromWhitelist(_beneficiary);
-    }
-
-    function claimApeXVIPNFT() external payable isWhitelisted(msg.sender) {
-        require(msg.value == price, "value not match");
-        totalEth = totalEth + price;
-        require(remainOwners > 0, "SOLD_OUT");
-        _mint(msg.sender, id);
-        require(block.timestamp <= startTime, "PLEASE_CLAIM_NFT_BEFORE_RELEASE_BEGIN");
-        id++;
-        remainOwners--;
-        _removeFromWhitelist(msg.sender);
-        buyer[msg.sender] = true;
-    }
-
-    function claimAPEX() public {
-        address user = msg.sender;
-        require(buyer[user], "ONLY_VIP_NFT_BUYER_CAN_CLAIM");
-
-        uint256 unClaimed = claimableAmount(user);
-        require(unClaimed > 0, "unClaimed_AMOUNT_MUST_BIGGER_THAN_ZERO");
-
-        claimed[user] = claimed[user] + unClaimed;
-
-        IERC20(token).transfer(user, unClaimed);
-
-        emit Claimed(user, unClaimed);
-    }
-
-    function claimableAmount(address user) public view returns (uint256) {
-        return vestedAmount() - (claimed[user]);
-    }
-
-    function vestedAmount() public view returns (uint256) {
-        if (block.timestamp < cliff) {
-            return 0;
-        } else if (block.timestamp >= duration) {
-            return totalAmount;
-        } else {
-            return (totalAmount * (block.timestamp - cliff)) / (duration - cliff);
+    function removeFromWhitelist(address[] calldata _beneficiaries) external onlyOwner {
+        for (uint256 i = 0; i < _beneficiaries.length; i++) {
+            delete whitelist[_beneficiaries[i]];
         }
     }
 
-    function _removeFromWhitelist(address _beneficiary) internal {
-        whitelist[_beneficiary] = false;
+    function withdrawETH(address to, uint256 amount) external onlyOwner {
+        payable(to).transfer(amount);
     }
 
-    function withdrawETH(address to) public onlyOwner {
-        payable(to).transfer(address(this).balance);
+    function claimApeXVIPNFT() external payable {
+        require(whitelist[msg.sender], "WHITELIST");
+        require(msg.value == price, "VALUE_NOT_MATCH");
+        require(id < nftMaxAmount, "SOLD_OUT");
+        require(block.timestamp <= startTime, "ENDED");
+        _mint(msg.sender, id);
+
+        id++;
+        delete whitelist[msg.sender];
+        buyer[msg.sender] = true;
     }
 
-    modifier isWhitelisted(address _beneficiary) {
-        require(whitelist[_beneficiary]);
-        _;
+    function claimAPEX() external {
+        address user = msg.sender;
+        require(buyer[user], "ONLY_VIP_NFT_BUYER_CAN_CLAIM");
+
+        uint256 claimable = _vestedAmount() - claimed[user];
+        require(claimable > 0, "CLAIMABLE_AMOUNT_MUST_BIGGER_THAN_ZERO");
+
+        claimed[user] = claimed[user] + claimable;
+
+        IERC20(token).transfer(user, claimable);
+
+        emit Claimed(user, claimable);
+    }
+
+    function claimableAmount(address user) external view returns (uint256) {
+        return _vestedAmount() - claimed[user];
+    }
+
+    function vestedAmount() external view returns (uint256) {
+        return _vestedAmount();
+    }
+
+    function _vestedAmount() internal view returns (uint256) {
+        if (block.timestamp <= cliffTime) {
+            return 0;
+        } else if (block.timestamp >= endTime) {
+            return totalAmount;
+        } else {
+            return (totalAmount * (block.timestamp - cliffTime)) / (endTime - cliffTime);
+        }
     }
 }
--- contracts/NFT/NftSquid.sol
+++ contracts/NFT/NftSquid.sol
@@ -9,16 +9,14 @@ contract NftSquid is ERC721PresetMinterPauserAutoId, Ownable {
     uint256 private constant HALF_YEAR = 180 days;
     uint256 private constant MULTIPLIER = 1e18;
     uint256 public startTime;
-    uint256 public remainOwners;
+    uint256 public existNftAmount;
     uint256 internal BONUS_PERPAX = 5000 * 10**18;
     uint256 internal BASE_AMOUNT = 10000 * 10**18;
     uint256 internal BURN_DISCOUNT = 40;
     uint256 public vault;
     //todo
     uint256 public price = 2.5 ether;
-    uint256 public id = 0;
     address public token;
-    uint256 public totalEth = 0;
 
     event Burn(uint256 tokenId, uint256 withdrawAmount, address indexed sender);
 
@@ -38,55 +36,55 @@ contract NftSquid is ERC721PresetMinterPauserAutoId, Ownable {
         startTime = _startTime; //unix time
     }
 
+    function withdrawETH(address to, uint256 amount) external onlyOwner {
+        payable(to).transfer(amount);
+    }
+
+    function withdrawERC20Token(address to, uint256 amount) external onlyOwner {
+        uint256 _balance = IERC20(token).balanceOf(address(this));
+        require(amount <= _balance && amount != 0, "nft.withdrawERC20Token: NO_ENOUGH_TOKEN");
+        IERC20(token).transfer(to, amount);
+    }
+
     // player can  buy before startTime
     function claimApeXNFT() external payable {
         require(msg.value == price, "value not match");
-        totalEth = totalEth + price;
-        _mint(msg.sender, id);
         require(block.timestamp < startTime, "GAME_IS_ALREADY_BEGIN");
-        id++;
-        remainOwners++;
+        _mint(msg.sender, existNftAmount);
+        existNftAmount++;
     }
 
     // player burn their nft
     function burnAndEarn(uint256 tokenId) external {
-        require(remainOwners > 0, "ALL_BURNED");
+        require(existNftAmount > 0, "ALL_BURNED");
         require(ownerOf(tokenId) == msg.sender, "NO_AUTHORITY");
         require(startTime != 0 && block.timestamp >= startTime, "GAME_IS_NOT_BEGIN");
         _burn(tokenId);
         uint256 withdrawAmount = calculateWithdrawAmount();
         emit Burn(tokenId, withdrawAmount, msg.sender);
-        require(IERC20(token).transfer(msg.sender, withdrawAmount));
+        IERC20(token).transfer(msg.sender, withdrawAmount);
     }
 
     function calculateWithdrawAmount() internal returns (uint256 withdrawAmount) {
-        uint256 endTime = startTime + HALF_YEAR;
+        uint256 _startTime = startTime;
+        uint256 endTime = _startTime + HALF_YEAR;
         uint256 nowTime = block.timestamp;
-        uint256 diffTime = nowTime < endTime ? nowTime - startTime : endTime - startTime;
+        require(nowTime >= _startTime, "NOT_STARTED");
+        uint256 diffTime = nowTime < endTime ? nowTime - _startTime : endTime - _startTime;
 
         // the last one is special
-        if (remainOwners == 1) {
+        if (existNftAmount == 1) {
             withdrawAmount = BASE_AMOUNT + BONUS_PERPAX + vault;
             return withdrawAmount;
         }
 
         // (t/6*5000+ vault/N)60%
-        uint256 bouns = (((diffTime * BONUS_PERPAX) / HALF_YEAR + vault / remainOwners) * 60) / 100;
+        uint256 bonus = (((diffTime * BONUS_PERPAX) / HALF_YEAR + vault / existNftAmount) * 60) / 100;
 
-        withdrawAmount = BASE_AMOUNT + bouns;
+        withdrawAmount = BASE_AMOUNT + bonus;
 
-        // vault += 5000-bouns (may negative)
-        vault = vault + BONUS_PERPAX - bouns;
-        remainOwners = remainOwners - 1;
-    }
-
-    function withdrawETH(address to) public onlyOwner {
-        payable(to).transfer(totalEth);
-    }
-
-    function withdrawERC20Token(address to) public onlyOwner returns (bool) {
-        require(IERC20(token).balanceOf(address(this)) >= 0);
-        require(IERC20(token).transfer(to, IERC20(token).balanceOf(address(this))));
-        return true;
+        // vault += 5000-bonus (may negative)
+        vault = vault + BONUS_PERPAX - bonus;
+        existNftAmount = existNftAmount - 1;
     }
 }
