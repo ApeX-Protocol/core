832f64523a2477b06a93ece3a9662207eb66ed6b
yan <yan.he@bybit.com>
2022-02-25 14:54:46 +0800

    fix the config

--- contracts/core/Config.sol
+++ contracts/core/Config.sol
@@ -9,7 +9,7 @@ import "../utils/Ownable.sol";
 contract Config is IConfig, Ownable {
     address public override priceOracle;
 
-    uint8 public override beta = 50; // 50-200，50 means 0.5
+    uint8 public override beta = 70; // 50-200，50 means 0.5
     uint256 public override maxCPFBoost = 10; // default 10
     uint256 public override rebasePriceGap = 5; //0-100 , if 5 means 5%
     uint256 public override tradingSlippage = 5; //0-100, if 5 means 5%
