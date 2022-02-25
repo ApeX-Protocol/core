69828b4ce0b56a985c7eee98805877f8af6e3a21
keeganlee <keegan.lee704@gmail.com>
2022-02-25 23:17:45 +0800

    update some .sol License-Identifier to MIT

--- contracts/core/FeeToTreasury.sol
+++ contracts/core/FeeToTreasury.sol
@@ -5,25 +5,34 @@ import "./interfaces/IERC20.sol";
 import "./interfaces/IAmm.sol";
 import "../utils/Ownable.sol";
 import "../libraries/TransferHelper.sol";
+import "../core/interfaces/uniswapV3/IUniswapV3Factory.sol";
+import "../core/interfaces/uniswapV3/IUniswapV3Pool.sol";
 
 contract FeeToTreasury is Ownable {
     event RecipientChanged(address indexed oldRecipient, address indexed newRecipient);
     event OperatorChanged(address indexed oldOperator, address indexed newOperator);
 
-    address public WETH;
     address public recipient;
     address public operator;
+
+    address public WETH;
+    address public v3Factory;
+    uint24[3] public v3Fees;
 
     modifier onlyOperator() {
         require(msg.sender == operator, "FORBIDDEN");
         _;
     }
 
-    constructor(address WETH_, address recipient_, address operator_) {
+    constructor(address WETH_, address v3Factory_, address recipient_, address operator_) {
         owner = msg.sender;
         WETH = WETH_;
+        v3Factory = v3Factory_;
         recipient = recipient_;
         operator = operator_;
+        v3Fees[0] = 500;
+        v3Fees[1] = 3000;
+        v3Fees[2] = 10000;
     }
 
     function setRecipient(address recipient_) external onlyOwner {
@@ -51,7 +60,21 @@ contract FeeToTreasury is Ownable {
     function swapETH(address[] memory tokens) external onlyOperator {
         for (uint256 i = 0; i < tokens.length; i++) {
             address token = tokens[i];
-
+            uint256 balance = IERC20(token).balanceOf(address(this));
+            if (token != WETH && balance > 0) {
+                address pool;
+                uint256 poolLiquidity;
+                for (uint256 j = 0; j < v3Fees.length; j++) {
+                    address tempPool = IUniswapV3Factory(v3Factory).getPool(token, WETH, v3Fees[i]);
+                    if (tempPool == address(0)) continue;
+                    uint256 tempLiquidity = uint256(IUniswapV3Pool(tempPool).liquidity());
+                    // use the max liquidity pool as target pool
+                    if (tempLiquidity > poolLiquidity) {
+                        poolLiquidity = tempLiquidity;
+                        pool = tempPool;
+                    }
+                }
+            }
         }
     }
 
