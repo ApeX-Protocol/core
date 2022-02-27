841adf763ead56142c7c66fe9dabca10a4f870df
keeganlee <keegan.lee704@gmail.com>
2022-02-27 19:08:45 +0800

    MockUniswapV3Pool add swap function

--- contracts/mocks/MockUniswapV3Pool.sol
+++ contracts/mocks/MockUniswapV3Pool.sol
@@ -80,6 +80,16 @@ contract MockUniswapV3Pool is IUniswapV3Pool {
         });
     }
 
+    function swap(
+        address recipient,
+        bool zeroForOne,
+        int256 amountSpecified,
+        uint160 sqrtPriceLimitX96,
+        bytes calldata data
+    ) external override returns (int256 amount0, int256 amount1) {
+
+    }
+
     function setLiquidity(uint128 liquidity_) external {
         liquidity = liquidity_;
     }
