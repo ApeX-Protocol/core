c7d83df6e83ec9c35ab28e8c5824c0b82ffcd628
keeganlee <keegan.lee704@gmail.com>
2022-02-27 16:53:17 +0800

    change FeeToTreasury to DAOTreasury

--- contracts/core/DAOTreasury.sol
+++ contracts/core/DAOTreasury.sol
@@ -5,10 +5,12 @@ import "./interfaces/IERC20.sol";
 import "./interfaces/IAmm.sol";
 import "../utils/Ownable.sol";
 import "../libraries/TransferHelper.sol";
+import "../libraries/TickMath.sol";
 import "../core/interfaces/uniswapV3/IUniswapV3Factory.sol";
 import "../core/interfaces/uniswapV3/IUniswapV3Pool.sol";
+import "../core/interfaces/IWETH.sol";
 
-contract FeeToTreasury is Ownable {
+contract DAOTreasury is Ownable {
     event RecipientChanged(address indexed oldRecipient, address indexed newRecipient);
     event OperatorChanged(address indexed oldOperator, address indexed newOperator);
 
@@ -57,15 +59,16 @@ contract FeeToTreasury is Ownable {
         }
     }
 
-    function swapETH(address[] memory tokens) external onlyOperator {
+    function batchSwapToETH(address[] memory tokens) external onlyOperator {
         for (uint256 i = 0; i < tokens.length; i++) {
             address token = tokens[i];
             uint256 balance = IERC20(token).balanceOf(address(this));
             if (token != WETH && balance > 0) {
+                // query target pool
                 address pool;
                 uint256 poolLiquidity;
                 for (uint256 j = 0; j < v3Fees.length; j++) {
-                    address tempPool = IUniswapV3Factory(v3Factory).getPool(token, WETH, v3Fees[i]);
+                    address tempPool = IUniswapV3Factory(v3Factory).getPool(token, WETH, v3Fees[j]);
                     if (tempPool == address(0)) continue;
                     uint256 tempLiquidity = uint256(IUniswapV3Pool(tempPool).liquidity());
                     // use the max liquidity pool as target pool
@@ -74,29 +77,37 @@ contract FeeToTreasury is Ownable {
                         pool = tempPool;
                     }
                 }
+
+                // swap token to WETH
+                bool zeroForOne = token < WETH;
+                IUniswapV3Pool(pool).swap(
+                    address(this),
+                    zeroForOne,
+                    int256(balance),
+                    zeroForOne ? TickMath.MIN_SQRT_RATIO + 1 : TickMath.MAX_SQRT_RATIO - 1,
+                    ""
+                );
             }
         }
+        uint256 wethBalance = IERC20(WETH).balanceOf(address(this));
+        IWETH(WETH).withdraw(wethBalance);
     }
 
     function withdrawUSDC(address[] memory amms) external onlyOwner {
 
     }
 
-    function getBestPool() internal {
-        // find out the pool with best liquidity as target pool
-        address pool;
-        address tempPool;
+    function _getPool(address token) internal view returns (address pool) {
         uint256 poolLiquidity;
-        uint256 tempLiquidity;
-        // for (uint256 i = 0; i < v3Fees.length; i++) {
-        //     tempPool = IUniswapV3Factory(v3Factory).getPool(baseToken, quoteToken, v3Fees[i]);
-        //     if (tempPool == address(0)) continue;
-        //     tempLiquidity = uint256(IUniswapV3Pool(tempPool).liquidity());
-        //     // use the max liquidity pool as index price source
-        //     if (tempLiquidity > poolLiquidity) {
-        //         poolLiquidity = tempLiquidity;
-        //         pool = tempPool;
-        //     }
-        // }
+        for (uint256 i = 0; i < v3Fees.length; i++) {
+            address tempPool = IUniswapV3Factory(v3Factory).getPool(token, WETH, v3Fees[i]);
+            if (tempPool == address(0)) continue;
+            uint256 tempLiquidity = uint256(IUniswapV3Pool(tempPool).liquidity());
+            // use the max liquidity pool as target pool
+            if (tempLiquidity > poolLiquidity) {
+                poolLiquidity = tempLiquidity;
+                pool = tempPool;
+            }
+        }
     }
 }
--- contracts/core/interfaces/uniswapV3/IUniswapV3Pool.sol
+++ contracts/core/interfaces/uniswapV3/IUniswapV3Pool.sol
@@ -2,6 +2,14 @@
 pragma solidity ^0.8.0;
 
 interface IUniswapV3Pool {
+    function swap(
+        address recipient,
+        bool zeroForOne,
+        int256 amountSpecified,
+        uint160 sqrtPriceLimitX96,
+        bytes calldata data
+    ) external returns (int256 amount0, int256 amount1);
+    
     function increaseObservationCardinalityNext(uint16 observationCardinalityNext) external;
     
     function slot0()
