83fafc04c049458cc6b3ada2bcfe52c4d866f4f8
Roy Blankman <blankman@boustro.com>
2022-03-18 01:06:11 +0100

    improving plotting

--- plot.sh
+++ plot.sh
@@ -1,11 +1,15 @@
 output=$1
 
+pnl=$(echo "$(awk -F', ' '{s+=$6}; END {print s}' $1) / 10^18" | bc -l)
+beta=$(echo $1 | awk -F'[^0-9]+' '{ print $2 }')
+
 gnuplot <<- EOF
   set terminal png size 2400,1800
   set output '$(basename ${output} .csv).png'
   set key outside
   set datafile separator ","
-  set title "Beta Simulation"
+  set title font ",22"
+  set title "Beta: $beta **** PnL: $pnl"
 
   plot '${output}' using 0:2 with lines title "Oracle Price", '' using 0:3 with lines title "Pool Price",\
        '' using 0:3:(10-\$1):(abs(\$1*2)) w points pt variable ps variable lc rgb 'blue' title "",\
