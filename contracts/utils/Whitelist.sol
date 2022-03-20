48eb4d05fb340ccd7fde8818f4c6bcaa4f3ac439
dectf <yuanye.li@bybit.com>
2022-03-20 21:47:25 +0800

    fix: split mint and burn for esApeX

--- contracts/utils/Whitelist.sol
+++ contracts/utils/Whitelist.sol
@@ -5,10 +5,7 @@ import "./Ownable.sol";
 
 abstract contract Whitelist is Ownable {
     mapping(address => bool) public whitelist;
-
-    function _addWhitelist(address account) internal onlyOwner {
-        whitelist[account] = true;
-    }
+    mapping(address => bool) public operator; //have access to mint/burn
 
     function addManyWhitelist(address[] calldata accounts) external onlyOwner {
         for (uint256 i = 0; i < accounts.length; i++) {
@@ -22,8 +19,27 @@ abstract contract Whitelist is Ownable {
         }
     }
 
-    modifier onlyWhitelist() {
-        require(whitelist[msg.sender], "whitelist: NOT_IN_WHITELIST");
+    function addOperator(address account) external onlyOwner {
+        _addOperator(account);
+    }
+
+    function removeOperator(address account) external onlyOwner {
+        require(operator[account], "whitelist.removeOperator: NOT_OPERATOR");
+        operator[account] = false;
+    }
+
+    function _addOperator(address account) internal {
+        require(!operator[account], "whitelist.addOperator: ALREADY_OPERATOR");
+        operator[account] = true;
+    }
+
+    modifier onlyOperator() {
+        require(operator[msg.sender], "whitelist: NOT_IN_OPERATOR");
+        _;
+    }
+
+    modifier operatorOrWhitelist() {
+        require(operator[msg.sender] || whitelist[msg.sender], "whitelist: NOT_IN_OPERATOR_OR_WHITELIST");
         _;
     }
 }
--- test/esApeX.js
+++ test/esApeX.js
@@ -12,33 +12,128 @@ describe("esApeX contract", function () {
     [owner, addr1, addr2] = await ethers.getSigners();
 
     const EsAPEX = await ethers.getContractFactory("EsAPEX");
-    esApeXToken = await EsAPEX.deploy(addr1.address);
+    esApeXToken = await EsAPEX.deploy(addr1.address); //addr1 is operator when init
   });
 
   describe("mint", function () {
-    it("can mint by whitelist operator", async function () {
+    it("can mint by operator", async function () {
       let oldBalance = (await esApeXToken.balanceOf(owner.address)).toNumber();
       await esApeXToken.connect(addr1).mint(owner.address, 100);
       let newBalance = (await esApeXToken.balanceOf(owner.address)).toNumber();
       expect(newBalance).to.be.equal(oldBalance + 100);
     });
 
-    it("revert when mint by non-whitelist operator", async function () {
-      await expect(esApeXToken.mint(owner.address, 100)).to.be.revertedWith("whitelist: NOT_IN_WHITELIST");
+    it("revert when mint by non-operator", async function () {
+      await expect(esApeXToken.mint(owner.address, 100)).to.be.revertedWith("whitelist: NOT_IN_OPERATOR");
     });
 
-    it("can mint by whitelist operator who is added", async function () {
-      await esApeXToken.addManyWhitelist([owner.address]);
+    it("can mint by new operator", async function () {
+      await esApeXToken.addOperator(owner.address);
       let oldBalance = (await esApeXToken.balanceOf(owner.address)).toNumber();
       await esApeXToken.mint(owner.address, 100);
       let newBalance = (await esApeXToken.balanceOf(owner.address)).toNumber();
       expect(newBalance).to.be.equal(oldBalance + 100);
     });
 
-    it("revert when mint by non-whitelist operator who is removed", async function () {
-      await esApeXToken.removeManyWhitelist([addr1.address]);
+    it("revert when mint by delete operator", async function () {
+      await esApeXToken.removeOperator(addr1.address);
       await expect(esApeXToken.connect(addr1).mint(owner.address, 100)).to.be.revertedWith(
-        "whitelist: NOT_IN_WHITELIST"
+        "whitelist: NOT_IN_OPERATOR"
+      );
+    });
+  });
+
+  describe("burn", function () {
+    it("can burn by operator", async function () {
+      await esApeXToken.connect(addr1).mint(owner.address, 100);
+      let oldBalance = (await esApeXToken.balanceOf(owner.address)).toNumber();
+      await esApeXToken.connect(addr1).burn(owner.address, 100);
+      let newBalance = (await esApeXToken.balanceOf(owner.address)).toNumber();
+      expect(newBalance).to.be.equal(oldBalance - 100);
+    });
+
+    it("revert when burn by non-operator", async function () {
+      await expect(esApeXToken.burn(owner.address, 100)).to.be.revertedWith("whitelist: NOT_IN_OPERATOR");
+    });
+
+    it("can burn by new operator", async function () {
+      await esApeXToken.addOperator(owner.address);
+      await esApeXToken.mint(owner.address, 100);
+      let oldBalance = (await esApeXToken.balanceOf(owner.address)).toNumber();
+      await esApeXToken.burn(owner.address, 100);
+      let newBalance = (await esApeXToken.balanceOf(owner.address)).toNumber();
+      expect(newBalance).to.be.equal(oldBalance - 100);
+    });
+
+    it("revert when burn by deleted operator", async function () {
+      await esApeXToken.removeOperator(addr1.address);
+      await expect(esApeXToken.connect(addr1).burn(owner.address, 100)).to.be.revertedWith(
+        "whitelist: NOT_IN_OPERATOR"
+      );
+    });
+  });
+
+  describe("transfer", function () {
+    it("can transfer by whitelist", async function () {
+      await esApeXToken.connect(addr1).mint(owner.address, 100);
+      await esApeXToken.addManyWhitelist([owner.address]);
+      let oldBalance = (await esApeXToken.balanceOf(owner.address)).toNumber();
+      await esApeXToken.transfer(addr1.address, 100);
+      let newBalance = (await esApeXToken.balanceOf(owner.address)).toNumber();
+      expect(newBalance).to.be.equal(oldBalance - 100);
+    });
+
+    it("can transfer by operator", async function () {
+      await esApeXToken.connect(addr1).mint(addr1.address, 100);
+      let oldBalance = (await esApeXToken.balanceOf(owner.address)).toNumber();
+      await esApeXToken.connect(addr1).transfer(owner.address, 100);
+      let newBalance = (await esApeXToken.balanceOf(owner.address)).toNumber();
+      expect(newBalance).to.be.equal(oldBalance + 100);
+    });
+
+    it("revert when transfer by non-whitelist and non-operator", async function () {
+      await expect(esApeXToken.transfer(owner.address, 100)).to.be.revertedWith(
+        "whitelist: NOT_IN_OPERATOR_OR_WHITELIST"
+      );
+    });
+
+    it("revert when transfer by deleted operator", async function () {
+      await esApeXToken.removeOperator(addr1.address);
+      await expect(esApeXToken.connect(addr1).transfer(owner.address, 100)).to.be.revertedWith(
+        "whitelist: NOT_IN_OPERATOR_OR_WHITELIST"
+      );
+    });
+  });
+
+  describe("transferFrom", function () {
+    it.only("can transferFrom by whitelist", async function () {
+      await esApeXToken.connect(addr1).mint(owner.address, 100);
+      await esApeXToken.addManyWhitelist([addr2.address]);
+      await esApeXToken.approve(addr2.address, 100);
+      let oldBalance = (await esApeXToken.balanceOf(owner.address)).toNumber();
+      await esApeXToken.connect(addr2).transferFrom(owner.address, addr1.address, 100);
+      let newBalance = (await esApeXToken.balanceOf(owner.address)).toNumber();
+      expect(newBalance).to.be.equal(oldBalance - 100);
+    });
+
+    it("can transfer by operator", async function () {
+      await esApeXToken.connect(addr1).mint(addr1.address, 100);
+      let oldBalance = (await esApeXToken.balanceOf(owner.address)).toNumber();
+      await esApeXToken.connect(addr1).transfer(owner.address, 100);
+      let newBalance = (await esApeXToken.balanceOf(owner.address)).toNumber();
+      expect(newBalance).to.be.equal(oldBalance + 100);
+    });
+
+    it("revert when transfer by non-whitelist and non-operator", async function () {
+      await expect(esApeXToken.transfer(owner.address, 100)).to.be.revertedWith(
+        "whitelist: NOT_IN_OPERATOR_OR_WHITELIST"
+      );
+    });
+
+    it("revert when transfer by deleted operator", async function () {
+      await esApeXToken.removeOperator(addr1.address);
+      await expect(esApeXToken.connect(addr1).transfer(owner.address, 100)).to.be.revertedWith(
+        "whitelist: NOT_IN_OPERATOR_OR_WHITELIST"
       );
     });
   });
