22b2bf9b4f8f3c2054d55d7531dab0afe94ea7dc
keeganlee <keegan.lee704@gmail.com>
2022-06-06 10:55:17 +0800

    deploy limit order in mainnet

--- scripts/deploy_limit_order.js
+++ scripts/deploy_limit_order.js
@@ -3,13 +3,13 @@ const { BigNumber } = require("@ethersproject/bignumber");
 const verifyStr = "npx hardhat verify --network";
 
 // prod
-// const pairFactoryAddr = "0xCE09a98C734ffB8e209b907FB0657193796FE3fD";
-// const wethAddress = "0x655e2b2244934Aea3457E3C56a7438C271778D44";
-// const botAddr = "0xbc6e4e0bc15293b5b9f0173c1c4a56525768d36c";
+const pairFactoryAddr = "0xA7B799832B46B51b2b6a156FDCE58525dE24Ac0f";
+const wethAddress = "0x82af49447d8a07e3bd95bd0d56f35241523fbab1";
+const botAddr = "0xa41e805010d338Cf909138DFE4dbB7Ee356d20A5";
 //test
-const pairFactoryAddr = "0xCE09a98C734ffB8e209b907FB0657193796FE3fD"; // dev
-const wethAddress = "0x655e2b2244934Aea3457E3C56a7438C271778D44"; // mockWETH
-const botAddr = "0xbc6e4e0bc15293b5b9f0173c1c4a56525768d36c";
+// const pairFactoryAddr = "0xCE09a98C734ffB8e209b907FB0657193796FE3fD"; // dev
+// const wethAddress = "0x655e2b2244934Aea3457E3C56a7438C271778D44"; // mockWETH
+// const botAddr = "0xbc6e4e0bc15293b5b9f0173c1c4a56525768d36c";
 
 let signer;
 let routerForKeeper;
