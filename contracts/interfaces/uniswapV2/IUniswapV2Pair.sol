55abcd7bec95ba8febb859c3e29ddad1e9a14589
keegan <keegan.lee704@gmail.com>
2023-03-03 21:28:24 +0800

    update

--- contracts/interfaces/uniswapV2/IUniswapV2Pair.sol
+++ contracts/interfaces/uniswapV2/IUniswapV2Pair.sol
@@ -3,9 +3,21 @@ pragma solidity ^0.8.0;
 
 interface IUniswapV2Pair {
     function token0() external view returns (address);
+
     function token1() external view returns (address);
+
     function totalSupply() external view returns (uint256);
-    function getReserves() external view returns (uint112 reserve0, uint112 reserve1, uint32 blockTimestampLast);
+
+    function getReserves()
+        external
+        view
+        returns (
+            uint112 reserve0,
+            uint112 reserve1,
+            uint32 blockTimestampLast
+        );
+
     function price0CumulativeLast() external view returns (uint256);
+
     function price1CumulativeLast() external view returns (uint256);
 }
